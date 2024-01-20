5f2c79ab93161f5f41edf515fe5b3de04e9ca9b2
kimich1218 <ickim1218@gmail.com>
2024-01-20 10:29:06 +0900

    로깅 및 데이터베이스 설정

--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const logging = require("./middleware/logging");
+
+router.use(logging);
 
 router.get("/", (req, res) => {
   res.send("Hello World");
