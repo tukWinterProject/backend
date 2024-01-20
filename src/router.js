5be6404ad8077fc88e3cc9f838f71bcb17ecac73
조 성현 <agllwy116@tukorea.ac.kr>
2024-01-20 17:02:01 +0900

    feat : 도메인 router 정의

--- src/router.js
+++ src/router.js
@@ -2,10 +2,20 @@ const express = require("express");
 const router = express.Router();
 const logging = require("./middleware/logging");
 
+const verify = require("./middleware/jwtVerify");
+
+// 웹 페이지의 controller
+const userController = require("./api/user/controller");
+
 router.use(logging);
 
 router.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// 유저 기능 도메인
+router.post("/api/user/register", userController.register);
+router.post("/api/user/login", userController.login);
+router.get("/api/user/mypage", verify, userController.mypage);
 
 module.exports = router;
