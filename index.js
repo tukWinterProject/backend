57a00c6806d20a3aea3df602be2339e2cdfde627
kimich1218 <ickim1218@gmail.com>
2024-01-20 13:00:36 +0900

    index 위치 변경 및 이름 수정

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 const router = require("./src/router");
 
-app.use("/", router);
-
 //JSON 형식의 본문을 처리하기 위한 미들웨어
 app.use(bodyParser.json());
 
 //URL 인코딩된 본문을 처리하기 위한 미들웨어 {폼 데이터}
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`웹서버 구동... ${port}`);
