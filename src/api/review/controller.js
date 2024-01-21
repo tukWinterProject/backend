87eb8d4b3ab54816b5dd9157129a302ba2360621
조성현 <agllwy116@naver.com>
2024-01-21 22:31:31 +0900

    feat : 리뷰 기능 api 구현

---
+++ src/api/review/controller.js
@@ -0,0 +1,34 @@
+const { create, show, showByUserID } = require("./repository");
+
+// 리뷰 생성하기
+exports.create = async (req, res) => {
+    const user = req.user;
+    const body = req.body;
+    const movie_id = req.params.id;
+
+    const result = await create(body.content, body.rating, user.id, movie_id);
+
+    if (result.affectedRows > 0) {
+        res.send({result: "success" , message: "리뷰 등록에 성공했습니다!"});
+    } else {
+        res.send({ result : "fail" });
+    }
+}
+
+// 영화 별 리뷰들 조회하기
+exports.show = async (req, res) => {
+    const id = req.params.id;
+
+    const items = await show(id);
+
+    res.send(items);
+}
+
+// 유저가 작성한 리뷰들 조회
+exports.showByUserID = async (req, res) => {
+    const user_id = req.user.id;
+
+    const items = await showByUserID(user_id);
+
+    res.send(items);
+}
