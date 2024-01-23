4277963b7651383960924e582a8c3b49604df85b
kimich <ickim1218@gmail.com>
2024-01-23 23:31:00 +0900

    영화리뷰할 때 닉네임까지 조회하도록 구현

--- src/api/review/controller.js
+++ src/api/review/controller.js
@@ -2,24 +2,24 @@ const { create, show } = require("./repository");
 
 // 리뷰 생성하기
 exports.create = async (req, res) => {
-    const user = req.user;
-    const body = req.body;
-    const movie_id = req.params.id;
+  const user = req.user;
+  const body = req.body;
+  const movie_id = req.params.id;
 
-    const result = await create(body.content, body.rating, user.id, movie_id);
+  const result = await create(body.content, body.rating, user.id, movie_id);
 
-    if (result.affectedRows > 0) {
-        res.send({result: "success" , message: "리뷰 등록에 성공했습니다!"});
-    } else {
-        res.send({ result : "fail" });
-    }
-}
+  if (result.affectedRows > 0) {
+    res.send({ result: "success", message: "리뷰 등록에 성공했습니다!" });
+  } else {
+    res.send({ result: "fail" });
+  }
+};
 
 // 영화 별 리뷰들 조회하기
 exports.show = async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const items = await show(id);
+  const items = await show(id);
 
-    res.send(items);
-}
+  res.send(items);
+};
--- src/api/review/repository.js
+++ src/api/review/repository.js
@@ -2,19 +2,22 @@ const { pool } = require("../../data");
 
 // 리뷰 생성
 exports.create = async (content, rating, user, movie) => {
-    const query = `INSERT INTO review
+  const query = `INSERT INTO review
     (content, rating, user_id, movie_id)
     VALUES (?, ?, ?, ?)`;
-    return await pool(query, [content, rating, user, movie]);
-}
+  return await pool(query, [content, rating, user, movie]);
+};
 
 // 영화 별 리뷰들 조회
 exports.show = async (id) => {
-    const query = `SELECT r.id, r.content, r.rating,
-    DATE_FORMAT(r.created_at, '%Y-%m-%d') AS created_at
-    FROM review r
-    JOIN movie m ON r.movie_id = m.id
-    WHERE r.movie_id = ?`;
-    let result = await pool(query, [id]);
-    return result.length < 0 ? null : result;
-}
+  const query = `
+        SELECT r.id, r.content, r.rating,
+        DATE_FORMAT(r.created_at, '%Y-%m-%d') AS created_at,
+        u.name AS user_name
+        FROM review r
+        JOIN movie m ON r.movie_id = m.id
+        JOIN user u ON r.user_id = u.id
+        WHERE r.movie_id = ?`;
+  let result = await pool(query, [id]);
+  return result.length < 0 ? null : result;
+};
