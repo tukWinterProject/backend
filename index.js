214f3569a7ecd0256814b5e200802cd4ee8c329c
kimich <ickim1218@gmail.com>
2024-01-23 16:28:12 +0900

    cors 추가

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require("./src/router");
+const cors = require("cors");
 
 //JSON 형식의 본문을 처리하기 위한 미들웨어
 app.use(bodyParser.json());
 
 //URL 인코딩된 본문을 처리하기 위한 미들웨어 {폼 데이터}
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use("/", router);
 
